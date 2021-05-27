f1652ac15392afec64f7a04964bb31a510bbda60
Rahul Badgujar <djrb2721999@gmail.com>
2021-05-27 16:24:44 +0530

    rb

---
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/ChequeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SalesAndInentoryWeb_Application.Models;
+namespace SalesAndInentoryWeb_Application.Controllers
+{
+    public class ChequeController : Controller
+    {
+        // GET: Cheque
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ChequeData()
+        {
+            using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
+            {
+                List<tbl_DebitNote> bank = db.tbl_DebitNote.ToList<tbl_DebitNote>();
+                return Json(new { data = bank }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
