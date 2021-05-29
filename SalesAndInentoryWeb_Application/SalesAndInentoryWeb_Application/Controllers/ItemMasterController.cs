2e59c84ec1d78b3f116a24532bf1195f76fa610f
Vithobamali <80039085+Vithobamali@users.noreply.github.com>
2021-05-29 09:29:58 +0530

    hiii

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/ItemMasterController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/ItemMasterController.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using DevExpress.Web.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SalesAndInentoryWeb_Application.Models;
+using System.Data.Entity;
 
 namespace SalesAndInentoryWeb_Application.Controllers
 {
@@ -14,9 +16,56 @@ namespace SalesAndInentoryWeb_Application.Controllers
         // GET: ItemMaster
         public ActionResult Index()
         {
-            return View(ew.tbl_ItemMaster.ToList());
+            return View();
 
         }
+
+        public ActionResult itemdata()
+        {
+            using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
+            {
+                List<tbl_ItemMaster> estimate = db.tbl_ItemMaster.ToList<tbl_ItemMaster>();
+                return Json(new { data = estimate }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new tbl_ItemMaster());
+            else
+            {
+                using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
+                {
+                    return View(db.tbl_ItemMaster.Where(x => x.ItemID == id).FirstOrDefault<tbl_ItemMaster>());
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(tbl_ItemMaster emp)
+        {
+            using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
+            {
+                if (emp.ItemID == 0)
+                {
+                    db.tbl_ItemMaster.Add(emp);
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    db.Entry(emp).State = EntityState.Modified;
+                    db.SaveChanges();
+                    return Json(new { success = true, message = "Updated Successfully" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+
+
+        }
+
 
         // GET: ItemMaster/Details/5
         public ActionResult Details(int id)
@@ -71,7 +120,13 @@ namespace SalesAndInentoryWeb_Application.Controllers
         // GET: ItemMaster/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
+            {
+                tbl_ItemMaster emp = db.tbl_ItemMaster.Where(x => x.ItemID == id).FirstOrDefault<tbl_ItemMaster>();
+                db.tbl_ItemMaster.Remove(emp);
+                db.SaveChanges();
+                return Json(new { success = true, message = "Deleted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
         }
 
         // POST: ItemMaster/Delete/5
@@ -89,5 +144,7 @@ namespace SalesAndInentoryWeb_Application.Controllers
                 return View();
             }
         }
+
+      
     }
 }
