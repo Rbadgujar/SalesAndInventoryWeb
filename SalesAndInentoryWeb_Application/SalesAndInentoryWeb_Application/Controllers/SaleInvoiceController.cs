8dfe77322b3b07727d46bb931e3c43b8c59cc8ab
Rahul Badgujar <djrb2721999@gmail.com>
2021-06-01 13:23:54 +0530

    rb

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/SaleInvoiceController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/SaleInvoiceController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using SalesAndInentoryWeb_Application.Models;
+
 namespace SalesAndInentoryWeb_Application.Controllers
 {
     public class SaleInvoiceController : Controller
@@ -11,13 +11,10 @@ namespace SalesAndInentoryWeb_Application.Controllers
         // GET: SaleInvoice
         public ActionResult Index()
         {
-           
             return View();
         }
-       
         public ActionResult SaleInvoice()
         {
-           
             return View();
         }
     }
