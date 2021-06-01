2e002602d0b9ade9bc9bd185d3bb3d06865046fc
Rahul Badgujar <djrb2721999@gmail.com>
2021-06-01 12:32:30 +0530

    rb

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Models/tbl_ItemMaster.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Models/tbl_ItemMaster.cs
@@ -11,7 +11,8 @@ namespace SalesAndInentoryWeb_Application.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.Data.Entity;
+
     public partial class tbl_ItemMaster
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -70,5 +71,9 @@ namespace SalesAndInentoryWeb_Application.Models
         public virtual ICollection<tbl_QuotationInner> tbl_QuotationInner { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tbl_SaleInvoiceInner> tbl_SaleInvoiceInner { get; set; }
+        public class CompanyListIdDBContext : DbContext
+        {
+            public DbSet<tbl_ItemMaster> Contacts { get; set; }
+        }
     }
 }
