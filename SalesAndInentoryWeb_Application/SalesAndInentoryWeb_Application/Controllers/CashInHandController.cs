77f05bffb11a415ce576445806f588f26381cefd
Rahul Badgujar <djrb2721999@gmail.com>
2021-06-09 15:24:51 +0530

    rb

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/CashInHandController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/CashInHandController.cs
@@ -4,36 +4,36 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SalesAndInentoryWeb_Application.Models;
-using System.Data.Entity;
 namespace SalesAndInentoryWeb_Application.Controllers
 {
-    public class DeliveryChallanController : Controller
+    public class CashInHandController : Controller
     {
-        // GET: DeliveryChallan
+        // GET: CashInHand
         public ActionResult Index()
         {
             return View();
+
         }
         public ActionResult Data()
         {
             using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
             {
                 db.Configuration.LazyLoadingEnabled = false;
-                List<tbl_DeliveryChallan> party = db.tbl_DeliveryChallan.ToList<tbl_DeliveryChallan>();
+                List<tbl_SaleInvoice> party = db.tbl_SaleInvoice.ToList<tbl_SaleInvoice>();
                 return Json(new { data = party }, JsonRequestBehavior.AllowGet);
             }
         }
-      
+
         [HttpGet]
         public ActionResult AddOrEdit(int id = 0)
         {
             if (id == 0)
-                return View(new tbl_DeliveryChallan());
+                return View(new tbl_CashAdjustment());
             else
             {
                 using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
                 {
-                    return View(db.tbl_DeliveryChallan.Where(x => x.ChallanNo == id).FirstOrDefault<tbl_DeliveryChallan>());
+                    return View(db.tbl_CashAdjustment.Where(x => x.ID == id).FirstOrDefault<tbl_CashAdjustment>());
                 }
             }
         }
--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/CompanyBankAccountController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/CompanyBankAccountController.cs
@@ -4,12 +4,11 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SalesAndInentoryWeb_Application.Models;
-using System.Data.Entity;
 namespace SalesAndInentoryWeb_Application.Controllers
 {
-    public class DeliveryChallanController : Controller
+    public class CompanyBankAccountController : Controller
     {
-        // GET: DeliveryChallan
+        // GET: CompanyBankAccount
         public ActionResult Index()
         {
             return View();
@@ -19,21 +18,21 @@ namespace SalesAndInentoryWeb_Application.Controllers
             using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
             {
                 db.Configuration.LazyLoadingEnabled = false;
-                List<tbl_DeliveryChallan> party = db.tbl_DeliveryChallan.ToList<tbl_DeliveryChallan>();
+                List<CompanyBankAccount> party = db.CompanyBankAccounts.ToList<CompanyBankAccount>();
                 return Json(new { data = party }, JsonRequestBehavior.AllowGet);
             }
         }
-      
+
         [HttpGet]
         public ActionResult AddOrEdit(int id = 0)
         {
             if (id == 0)
-                return View(new tbl_DeliveryChallan());
+                return View(new CompanyBankAccount());
             else
             {
                 using (idealtec_inventoryEntities10 db = new idealtec_inventoryEntities10())
                 {
-                    return View(db.tbl_DeliveryChallan.Where(x => x.ChallanNo == id).FirstOrDefault<tbl_DeliveryChallan>());
+                    return View(db.CompanyBankAccounts.Where(x => x.ID == id).FirstOrDefault<CompanyBankAccount>());
                 }
             }
         }
