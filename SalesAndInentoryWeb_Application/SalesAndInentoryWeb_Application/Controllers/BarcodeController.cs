6aa38d0865fde50cb9353286aa3c30a3496b6520
Vithobamali <80039085+Vithobamali@users.noreply.github.com>
2021-06-23 10:39:11 +0530

    hii

---
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/BarcodeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SalesAndInentoryWeb_Application.Controllers
+{
+    public class BarcodeController : Controller
+    {
+        // GET: Barcode
+        public ActionResult index()
+        {
+            return View();
+        }
+        public ActionResult BarcodePrint()
+        {
+            return View();
+        }
+    }
+}
