fc0510093ce1ff6d3a69c218b5b00371ae7f4ac7
Rahul Badgujar <djrb2721999@gmail.com>
2021-06-28 17:04:53 +0530

    rb

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/CompanyController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/CompanyController.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using SalesAndInentoryWeb_Application;
+using SalesAndInentoryWeb_Application.Models;
 namespace SalesAndInentoryWeb_Application.Controllers
 {
     public class CompanyController : Controller
     {
         // GET: Company
+        CompanyDataClassDataContext db = new CompanyDataClassDataContext();
         public ActionResult Index()
         {
             return View();
@@ -17,6 +19,14 @@ namespace SalesAndInentoryWeb_Application.Controllers
         {
             return View();
                  
+        }
+        [HttpGet]
+        public ActionResult showdata()
+        {
+          
+            var getdata = db.tbl_CompanyMasterSelect("Select",null,null,null,null, null, null, null, null, null, null, null, null, null, null, null, null, null).ToList();
+            return Json(new { data = getdata }, JsonRequestBehavior.AllowGet);
+
         }
         public ActionResult Company()
         {
