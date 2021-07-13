ba68bd863f21b7b91dbfa436044d50d57f4087c8
Idealtechinfo-9 <Idealtechinfo-9@DESKTOP-MCDLANS>
2021-07-13 18:24:00 +0530

    sale insert

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/EstimateQuatationController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/EstimateQuatationController.cs
@@ -32,12 +32,13 @@ namespace SalesAndInentoryWeb_Application.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddOrEdit(tbl_QuotationSelectResult esti)
+        public ActionResult AddOrEdit(tbl_QuotationSelectResult esti, tbl_QuotationInnerspResult inner)
         {
             try
             {
                 //CustomerName as PartyName,PaymentType,ReceiptNo,Date,Description,ReceivedAmount, UnusedAmount,Total,Status,image
                 db.tbl_QuotationSelect("Insert", null, esti.PartyName, esti.BillingAddress, Convert.ToDateTime(esti.Date), esti.StateofSupply, esti.ContactNo, esti.Description, esti.Image, esti.Tax1, esti.TaxAmount1,esti.CGST,esti.SGST, esti.TotalDiscount, esti.DiscountAmount1, esti.RoundFigure, esti.Total, null, null, null, null, null, esti.Status, null, null, esti.Barcode, esti.Company_ID, esti.ItemCategory, esti.CalTotal, esti.TaxShow, esti.Discount);
+                db.tbl_QuotationInnersp("Insert", null, inner.ItemID, inner.CategoryType, inner.ItemName, inner.BasicUnit, inner.ItemCode, inner.SalePrice, inner.TaxForSale, inner.SaleTaxAmount, inner.Qty, inner.freeQty, inner.ItemAmount, inner.Discount, inner.DiscountAmount, inner.Company_ID, inner.RefNo, inner.CGST, inner.SGST, inner.IGST, inner.CalTotal );
                 db.SubmitChanges();
                 return RedirectToAction("Index");
                 //return Json(new { success = true, message = "Saved Data Successfully" }, JsonRequestBehavior.AllowGet);
--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/ExpenceController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/ExpenceController.cs
@@ -16,11 +16,12 @@ namespace SalesAndInentoryWeb_Application.Controllers
             return View();
         }
 
-       [HttpGet]
-       public ActionResult Expencescategory()
+        [HttpGet]
+        public ActionResult Expencescategory()
         {
             return View();
         }
+
 		[HttpGet]
         public ActionResult ExpenceData()
         { 
@@ -29,6 +30,35 @@ namespace SalesAndInentoryWeb_Application.Controllers
 
 		}
 
-         
+        [HttpGet]
+        public ActionResult AddOrEdit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(tbl_ExpensesSelectResult exp)
+        {
+            try
+            {
+                //CustomerName as PartyName,PaymentType,ReceiptNo,Date,Description,ReceivedAmount, UnusedAmount,Total,Status,image
+                db.tbl_ExpensesSelect("Insert", null, exp.ExpenseCategory, exp.Date, exp.Description, exp.Image, exp.Total, exp.Paid, exp.Balance, exp.AdditinalFeild1, exp.AdditionalFeild2, exp.Status, exp.TableName, exp.compid);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+                //return Json(new { success = true, message = "Saved Data Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception e)
+            {
+                return View("Error", new HandleErrorInfo(e, "Expence", "AddOrEdit"));
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var tb = db.tbl_ExpensesSelect("Delete", id, null, null, null, null, null, null, null, null, null, null, null, null).ToList();
+            db.SubmitChanges();
+            return Json(new { success = true, message = "Delete Data Successfully" }, JsonRequestBehavior.AllowGet);
+        }
     }
 }
