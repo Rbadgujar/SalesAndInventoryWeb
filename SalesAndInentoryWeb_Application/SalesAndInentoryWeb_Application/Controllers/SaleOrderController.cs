5c51177fbde0e4e8b86cb3ea5fbaedea31cde82c
Rahul Badgujar <djrb2721999@gmail.com>
2021-07-16 17:52:38 +0530

    rb

--- SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/SaleOrderController.cs
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/Controllers/SaleOrderController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using SalesAndInentoryWeb_Application.Models;
 using System.Data.Entity;
-
+using SalesAndInentoryWeb_Application.ViewModel;
 namespace SalesAndInentoryWeb_Application.Controllers
 {
     public class SaleOrderController : Controller
@@ -34,31 +34,17 @@ namespace SalesAndInentoryWeb_Application.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddOrEdit(tbl_SaleOrderSelectResult order)
-        {
-            try
-            {
-                 object id1;
-
-                //CustomerName as PartyName,PaymentType,ReceiptNo,Date,Description,ReceivedAmount, UnusedAmount,Total,Status,image
-                id1 =   db.tbl_SaleOrderSelect("Insert", null, order.PartyName,order.BillingName, order.ContactNo, Convert.ToDateTime(order.OrderDate), order.DueDate, order.StateofSupply, order.PaymentType, order.TransportName, order.DeliveryLocation, order.VehicleNumber, order.Deliverydate, order.Description, order.TransportCharges, order.Image, order.Tax1, order.CGST, order.SGST, order.TaxAmount1, Convert.ToString(order.TotalDiscount), order.DiscountAmount1, order.RoundFigure, order.Total, order.Received, order.RemainingBal, order.PaymentTerms, null, null, null, null, null,  order.Status, null, null, order.ItemCategory, order.Barcode, order.IGST, order.Company_ID,  order.TaxShow, null,order.CalTotal);
-                db.SubmitChanges();
-               // db.tbl("Insert",id1, order.PartyName, order.BillingName, order.ContactNo, Convert.ToDateTime(order.OrderDate), order.DueDate, order.StateofSupply, order.PaymentType, order.TransportName, order.DeliveryLocation, order.VehicleNumber, order.Deliverydate, order.Description, order.TransportCharges, order.Image, order.Tax1, order.CGST, order.SGST, order.TaxAmount1, Convert.ToString(order.TotalDiscount), order.DiscountAmount1, order.RoundFigure, order.Total, order.Received, order.RemainingBal, order.PaymentTerms, null, null, null, null, null, order.Status, null, null, order.ItemCategory, order.Barcode, order.IGST, order.Company_ID, order.TaxShow, null, order.CalTotal);
-                db.SubmitChanges();
-                return RedirectToAction("SaleOrder");
-                //return Json(new { success = true, message = "Saved Data Successfully" }, JsonRequestBehavior.AllowGet);
-            }
-            catch (Exception e)
-            {
-                return View("Error", new HandleErrorInfo(e, "SaleOrder", "AddOrEdit"));
-            }
+        public ActionResult AddOrEdit(PartyDetails objpartydetails)
+        {                                          
+            //return Json(data: new {msg= "Data sucessfully inserted", status=true}, JsonRequestBehavior.AllowGet);
+            return Json(new { success = true, message = "Delete Data Successfully" }, JsonRequestBehavior.AllowGet);
         }
         [HttpPost]
         public ActionResult Delete(int id)
         {
             var getdata = db.tbl_SaleOrderSelect("Delete", id, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null).ToList();
             db.SubmitChanges();
-            return Json(new { success = true, message = "Delete Data Successfully" }, JsonRequestBehavior.AllowGet);
+            return Json(new { success = true, message = "Delete Data Successfully" }, JsonRequestBehavior.AllowGet);           
         }
     }
 }
---
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/ViewModel/ItemDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesAndInentoryWeb_Application.ViewModel
+{
+    public class ItemDetails
+    {
+        public int ItemID { get; set; }
+        public string ItemName { get; set; }
+        public int SalePrice { get; set; }
+
+      public int OrderNo { get; set; }
+    }
+}
