d702d9a794d1c3181cf0aeb453604023a4214c61
Rahul Badgujar <djrb2721999@gmail.com>
2021-08-03 18:23:01 +0530

    rb

---
+++ SalesAndInentoryWeb_Application/SalesAndInentoryWeb_Application/ViewModel/PartyDetailsDeliveryChallan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SalesAndInentoryWeb_Application.ViewModel
+{
+    public class PartyDetailsDeliveryChallan
+    {
+        public int ChallanNo { get; set; }
+        public string PartyName { get; set; }
+        public string BillingName { get; set; }
+        public string ContactNo { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public string StateOfSupply { get; set; }
+        public string PaymentType { get; set; }
+        public string TransportName { get; set; }
+        public string DeliveryLocation { get; set; }
+        public string VehicleNumber { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string Barcode { get; set; }
+        public double Received { get; set; }
+        public double RemainingBal { get; set; }
+        public double CalTotal { get; set; }
+        public string Status { get; set; }
+
+        public List<ItemPurchase> ListOfDeliveryChallan { get; set; }
+    }
+}
